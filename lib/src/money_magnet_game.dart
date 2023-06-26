548c95ac70fc6b44041db31a84acd3df4f735e9b
Yurii Antonov <yurii.antonov@globallogic.com>
2023-06-26 17:56:31 +0300

    Initial commit

---
+++ lib/src/money_magnet_game.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class MoneyMagnetGame extends StatefulWidget {
+  const MoneyMagnetGame({Key? key}) : super(key: key);
+
+  @override
+  _MoneyMagnetGameState createState() => _MoneyMagnetGameState();
+}
+
+class _MoneyMagnetGameState extends State<MoneyMagnetGame> {
+  int moneyCount = 0;
+  double magnetPower = 1.0;
+  double magnetRadius = 50.0;
+  double magnetSpeed = 1.0;
+
+  bool isGameStarted = false;
+
+  void startGame() {
+    setState(() {
+      isGameStarted = true;
+      moneyCount = 0;
+    });
+  }
+
+  void increaseMoney() {
+    setState(() {
+      moneyCount++;
+    });
+  }
+
+  void upgradeMagnetPower() {
+    setState(() {
+      if (moneyCount >= 10) {
+        moneyCount -= 10;
+        magnetPower += 0.5;
+      }
+    });
+  }
+
+  void upgradeMagnetRadius() {
+    setState(() {
+      if (moneyCount >= 10) {
+        moneyCount -= 10;
+        magnetRadius += 10.0;
+      }
+    });
+  }
+
+  void upgradeMagnetSpeed() {
+    setState(() {
+      if (moneyCount >= 10) {
+        moneyCount -= 10;
+        magnetSpeed += 0.5;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Money Magnet'),
+      ),
+      body: Center(
+        child: isGameStarted
+            ? Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Money count: $moneyCount',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            SvgPicture.asset(
+              'assets/images/magnet.svg',
+              height: 100,
+              width: 100,
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Upgrade Magnet',
+              style: TextStyle(fontSize: 20),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: upgradeMagnetPower,
+                  child: Text('Upgrade Power (10)'),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: upgradeMagnetRadius,
+                  child: Text('Upgrade Radius (10)'),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: upgradeMagnetSpeed,
+                  child: Text('Upgrade Speed (10)'),
+                ),
+              ],
+            ),
+          ],
+        )
+            : ElevatedButton(
+          onPressed: startGame,
+          child: Text('Start Game'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/play_session/play_session_screen.dart
@@ -0,0 +1,127 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:logging/logging.dart' hide Level;
+import 'package:provider/provider.dart';
+
+import '../ads/ads_controller.dart';
+import '../audio/audio_controller.dart';
+import '../audio/sounds.dart';
+import '../game_internals/level_state.dart';
+import '../games_services/games_services.dart';
+import '../games_services/score.dart';
+import '../in_app_purchase/in_app_purchase.dart';
+import '../level_selection/levels.dart';
+import '../money_magnet_game.dart';
+import '../player_progress/player_progress.dart';
+import '../style/confetti.dart';
+import '../style/palette.dart';
+
+class PlaySessionScreen extends StatefulWidget {
+  final GameLevel level;
+
+  const PlaySessionScreen(this.level, {Key? key}) : super(key: key);
+
+  @override
+  _PlaySessionScreenState createState() => _PlaySessionScreenState();
+}
+
+class _PlaySessionScreenState extends State<PlaySessionScreen> {
+  static final _log = Logger('PlaySessionScreen');
+
+  static const _celebrationDuration = Duration(milliseconds: 2000);
+  static const _preCelebrationDuration = Duration(milliseconds: 500);
+
+  bool _duringCelebration = false;
+  late DateTime _startOfPlay;
+
+  @override
+  void initState() {
+    super.initState();
+    _startOfPlay = DateTime.now();
+
+    // Preload ad for the win screen.
+    final adsRemoved = context.read<InAppPurchaseController?>()?.adRemoval.active ?? false;
+    if (!adsRemoved) {
+      final adsController = context.read<AdsController?>();
+      adsController?.preloadAd();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final palette = context.watch<Palette>();
+
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => LevelState(
+            goal: widget.level.difficulty,
+            onWin: _playerWon,
+          ),
+        ),
+      ],
+      child: IgnorePointer(
+        ignoring: _duringCelebration,
+        child: Scaffold(
+          backgroundColor: palette.backgroundPlaySession,
+          body: Stack(
+            children: [
+              MoneyMagnetGame(), // Використовуємо MoneyMagnetGame як частину інтерфейсу
+
+              SizedBox.expand(
+                child: Visibility(
+                  visible: _duringCelebration,
+                  child: IgnorePointer(
+                    child: Confetti(isStopped: !_duringCelebration),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _playerWon() async {
+    _log.info('Level ${widget.level.number} won');
+
+    final score = Score(
+      widget.level.number,
+      widget.level.difficulty,
+      DateTime.now().difference(_startOfPlay),
+    );
+
+    final playerProgress = context.read<PlayerProgress>();
+    playerProgress.setLevelReached(widget.level.number);
+
+    await Future<void>.delayed(_preCelebrationDuration);
+    if (!mounted) return;
+
+    setState(() {
+      _duringCelebration = true;
+    });
+
+    final audioController = context.read<AudioController>();
+    audioController.playSfx(SfxType.congrats);
+
+    final gamesServicesController = context.read<GamesServicesController?>();
+    if (gamesServicesController != null) {
+      if (widget.level.awardsAchievement) {
+        await gamesServicesController.awardAchievement(
+          android: widget.level.achievementIdAndroid!,
+          iOS: widget.level.achievementIdIOS!,
+        );
+      }
+
+      await gamesServicesController.submitLeaderboardScore(score);
+    }
+
+    await Future<void>.delayed(_celebrationDuration);
+    if (!mounted) return;
+
+    GoRouter.of(context).go('/play/won', extra: {'score': score});
+  }
+}
